9c62803f42da9c0e6d31716801c4270d104573cc
Andy de Vivanco <andy.dv.cm@gmail.com>
2022-07-02 16:37:40 -0500

    Prepare/Interview_Preparation_Kits/1_Week_Preparation_Kit/Day_2/ (second commit)

---
+++ Prepare/Interview_Preparation_Kits/1_Week_Preparation_Kit/Day_2/Counting_Sort_1/Counting_Sort_1_solution.py
@@ -0,0 +1,42 @@
+
+# https://www.hackerrank.com/challenges/one-week-preparation-kit-countingsort1/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    r = [0]*100
+    
+    for i in range(0, len(arr)):
+        if (arr[i] < 100):    # this conidtion is necessary 
+            r[arr[i]] += 1
+        
+    return r
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
