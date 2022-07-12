110fbcd3031ce008430df5e6b4c8a473f5aff8ef
Andy de Vivanco <andy.dv.cm@gmail.com>
2022-07-12 11:13:18 -0500

    Cesar's cypher solution

--- Prepare/Interview_Preparation_Kits/1_Week_Preparation_Kit/Day_3/Caesar_Cipher/Caesar_Cipher_solution.py
+++ Prepare/Interview_Preparation_Kits/1_Week_Preparation_Kit/Day_3/Caesar_Cipher/Caesar_Cipher_solution.py
@@ -17,20 +17,25 @@ import sys
 
 def caesarCipher(s, k):
     r_list = list(s)
-    for i in range(len(s)):
-        curr_ord = ord(s[i])
+    for i in range(len(r_list)):
+        curr_ord = ord(r_list[i])
+        k = k % 26
         if (curr_ord >= 65) & (curr_ord <= 90):
             new_ord = curr_ord + k
             if (new_ord > 90):
-                new_ord = 65 + (new_ord % 90 -1)
+                new_ord = new_ord - 26
+                
             r_list[i] = chr(new_ord) 
         elif(curr_ord >= 97) & (curr_ord <= 122):
             new_ord = curr_ord + k
             if (new_ord > 122):
-                new_ord = 97 + (new_ord %122 -1)
+                new_ord = new_ord - 26
+                
             r_list[i] = chr(new_ord) 
+        else:
+            r_list[i] = chr(curr_ord) 
 
-    return  ''.join(r_list)  
+    return ''.join(r_list)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -46,4 +51,3 @@ if __name__ == '__main__':
     fptr.write(result + '\n')
 
     fptr.close()
-
